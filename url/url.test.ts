5a7693862ec92e90031302c8bcacdce664b23626
stepbirt <stepbirt@gmail.com>
2024-11-22 10:56:00 +0700

    fix: remove log

--- url/url.test.ts
+++ url/url.test.ts
@@ -5,7 +5,6 @@ describe("short url", () => {
   test("get short url mush get original url", async () => {
     const shortenUrl = await shortUrl({ url: "www.test.com" });
     const resp = await getUrl({ id: shortenUrl.id });
-    console.log(resp);
     expect(resp.url).toBe("www.test.com");
   });
 });
